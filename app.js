3646c3d19927f18ed88c7716e0fcee601801947f
Juan Camilo Zapata <jczapataa@sura.com.co>
2022-03-25 15:02:04 -0500

    se implementa captura de errores

--- app.js
+++ app.js
@@ -8,16 +8,12 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/api', (request, response) => {
-  console.log(request.ip)
-  console.log(request.ips)
-  console.log(request.originalUrl)
+  console.info(request.ip)
+  console.info(request.ips)
+  console.info(request.originalUrl)
   response.send('<h1>Server is up!!!</h1>')
 })
 
 app.use('/api/items', ItemsController)
-
-app.use(function (err, req, res, next) {
-  res.status(err.message).end()
-})
 
 module.exports = app
--- routes/itemsController.js
+++ routes/itemsController.js
@@ -1,143 +1,155 @@
-const express = require('express');
-const fetch = require('node-fetch');
-const router = express.Router();
-const config = require('../config/settings');
-const okStatus = 200;
-const defaultLimit = '4';
+const express = require('express')
+const fetch = require('node-fetch')
+const router = express.Router()
+const config = require('../config/settings')
+const okStatus = 200
+const defaultLimit = '4'
 
 const author = {
   name: 'Juan Camilo',
-  lastname: 'Zapata',
-};
-
-router.get('/', function (request, response) {
-  getItemsBySearch(request, response);
-});
-
-router.get('/:id', function (request, response) {
-  getItemsById(request, response);
-});
-
-function badRequest(response) {
-  response.status(400).send('Bad request').end;
+  lastname: 'Zapata'
 }
 
-async function getItemsBySearch(request, response) {
+router.get('/', getItemsBySearch)
+
+router.get('/:id', getItemsById)
+
+function badRequest (response) {
+  response.status(400).send({
+    status: 400,
+    error: 'Bad Request'
+  }).end
+}
+
+async function getItemsBySearch (request, response) {
   if (JSON.stringify(request.query) !== '{}' && request.query.q) {
-    const limit = request.query.limit ?? defaultLimit;
-    const queryRequest = request.query.q.toLowerCase();
+    try {
+      const limit = request.query.limit ?? defaultLimit
+      const queryRequest = request.query.q.toLowerCase()
 
-    const itemsResponseRaw = await fetch(
-      `${config.apiMELI}/sites/MLA/search?q=${queryRequest}&limit=${limit}`
-    );
+      const itemsResponseRaw = await fetch(
+        `${config.apiMELI}/sites/MLA/search?q=${queryRequest}&limit=${limit}`
+      )
 
-    if (itemsResponseRaw.status === okStatus) {
-      let categoriesList = [];
-      const itemsResponseJSON = await itemsResponseRaw.json();
-      const itemsResult = itemsResponseJSON.results.map(item => {
+      if (itemsResponseRaw.status === okStatus) {
+        let categoriesList = []
+        const itemsResponseJSON = await itemsResponseRaw.json()
+        const itemsResult = itemsResponseJSON.results.map(item => {
+          const items = {
+            id: item.id,
+            title: item.title,
+            price: {
+              currency: item.prices.prices[0].currency_id,
+              amount: item.price,
+              decimals: 0
+            },
+            picture: item.thumbnail,
+            condition: item.condition,
+            free_shipping: item.shipping.free_shipping,
+            stateName: item.address.state_name
+          }
+          return items
+        })
+
+        const categoriesResults = itemsResponseJSON.available_filters.filter(
+          cat => cat.id === 'category'
+        )
+
+        if (categoriesResults.length !== 0) {
+          const maxCategory = categoriesResults[0].values.reduce(
+            (previousValue, currentValue) =>
+              previousValue.results > currentValue.results
+                ? previousValue
+                : currentValue
+          )
+
+          const itemCategoriesResponseRaw = await fetch(
+            `${config.apiMELI}/categories/${maxCategory.id}`
+          )
+
+          if (itemCategoriesResponseRaw.status === okStatus) {
+            const itemCategoriesJSON = await itemCategoriesResponseRaw.json()
+            categoriesList = itemCategoriesJSON.path_from_root.map(
+              cat => cat.name
+            )
+          }
+        }
+
         const items = {
-          id: item.id,
-          title: item.title,
-          price: {
-            currency: item.prices.prices[0].currency_id,
-            amount: item.price,
-            decimals: 0,
-          },
-          picture: item.thumbnail,
-          condition: item.condition,
-          free_shipping: item.shipping.free_shipping,
-          stateName: item.address.state_name,
-        };
-        return items;
-      });
+          author: author,
+          categories: categoriesList,
+          items: itemsResult
+        }
 
-      const categoriesResults = itemsResponseJSON.available_filters.filter(
-        cat => cat.id === 'category'
-      );
+        response.status(okStatus).json(items)
+      } else {
+        response
+          .status(itemsResponseRaw.status)
+          .send('Status ' + itemsResponseRaw.status).end
+      }
+    } catch (error) {
+      console.error(error)
+      response.status(error.message).end()
+    }
+  } else badRequest(response)
+}
 
-      if (categoriesResults.length !== 0) {
-        const maxCategory = categoriesResults[0].values.reduce(
-          (previousValue, currentValue) =>
-            previousValue.results > currentValue.results
-              ? previousValue
-              : currentValue
-        );
+async function getItemsById (request, response) {
+  const id = request.params.id
+
+  try {
+    const itemResponseRaw = await fetch(`${config.apiMELI}/items/${id}`)
+
+    if (itemResponseRaw.status === okStatus) {
+      const itemDescriptionResponseRaw = await fetch(
+        `${config.apiMELI}/items/${id}/description`
+      )
+      if (itemDescriptionResponseRaw.status === okStatus) {
+        const itemResponseJSON = await itemResponseRaw.json()
+        const itemDescriptionResponseJSON =
+          await itemDescriptionResponseRaw.json()
+        let categoriesList = []
 
         const itemCategoriesResponseRaw = await fetch(
-          `${config.apiMELI}/categories/${maxCategory.id}`
-        );
+          `${config.apiMELI}/categories/${itemResponseJSON.category_id}`
+        )
 
         if (itemCategoriesResponseRaw.status === okStatus) {
-          const itemCategoriesJSON = await itemCategoriesResponseRaw.json();
+          const itemCategoriesJSON = await itemCategoriesResponseRaw.json()
           categoriesList = itemCategoriesJSON.path_from_root.map(
             cat => cat.name
-          );
+          )
         }
+
+        const item = {
+          author: author,
+          categories: categoriesList,
+          item: {
+            id: itemResponseJSON.id,
+            title: itemResponseJSON.title,
+            price: {
+              currency: itemResponseJSON.currency_id,
+              amount: itemResponseJSON.price,
+              decimals: 0
+            },
+            picture: itemResponseJSON.pictures[0].url,
+            condition: itemResponseJSON.condition,
+            free_shipping: itemResponseJSON.shipping.free_shipping,
+            sold_quantity: itemResponseJSON.sold_quantity,
+            description: itemDescriptionResponseJSON.plain_text
+          }
+        }
+        response.status(okStatus).json(item)
+      } else {
+        response
+          .status(itemResponseRaw.status)
+          .send('Status ' + itemResponseRaw.status).end
       }
-
-      const items = {
-        author: author,
-        categories: categoriesList,
-        items: itemsResult,
-      };
-
-      response.status(okStatus).json(items);
-    } else {
-      response
-        .status(itemsResponseRaw.status)
-        .send('Status ' + itemsResponseRaw.status).end;
     }
-  } else badRequest(response);
-}
-
-async function getItemsById(request, response) {
-  const id = request.params.id;
-  const itemResponseRaw = await fetch(`${config.apiMELI}/items/${id}`);
-
-  if (itemResponseRaw.status === okStatus) {
-    const itemDescriptionResponseRaw = await fetch(
-      `${config.apiMELI}/items/${id}/description`
-    );
-    if (itemDescriptionResponseRaw.status === okStatus) {
-      const itemResponseJSON = await itemResponseRaw.json();
-      const itemDescriptionResponseJSON =
-        await itemDescriptionResponseRaw.json();
-      let categoriesList = [];
-
-      const itemCategoriesResponseRaw = await fetch(
-        `${config.apiMELI}/categories/${itemResponseJSON.category_id}`
-      );
-
-      if (itemCategoriesResponseRaw.status === okStatus) {
-        const itemCategoriesJSON = await itemCategoriesResponseRaw.json();
-        categoriesList = itemCategoriesJSON.path_from_root.map(cat => cat.name);
-      }
-
-      const item = {
-        author: author,
-        categories: categoriesList,
-        item: {
-          id: itemResponseJSON.id,
-          title: itemResponseJSON.title,
-          price: {
-            currency: itemResponseJSON.currency_id,
-            amount: itemResponseJSON.price,
-            decimals: 0,
-          },
-          picture: itemResponseJSON.pictures[0].url,
-          condition: itemResponseJSON.condition,
-          free_shipping: itemResponseJSON.shipping.free_shipping,
-          sold_quantity: itemResponseJSON.sold_quantity,
-          description: itemDescriptionResponseJSON.plain_text,
-        },
-      };
-      response.status(okStatus).json(item);
-    } else {
-      response
-        .status(itemResponseRaw.status)
-        .send('Status ' + itemResponseRaw.status).end;
-    }
+  } catch (error) {
+    console.error(error)
+    response.status(error.message).end()
   }
 }
 
-module.exports = router;
+module.exports = router
